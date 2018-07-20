63b7fef990745617cda5bde77343ef922f1acf1d
sebastien <sebastien.helle@etsmtl.ca>
2018-07-20 15:27:41 -0400

    final

--- include/dif_drv_robot/hardware_interface.hpp
+++ include/dif_drv_robot/hardware_interface.hpp
@@ -1,40 +1,75 @@
+#ifndef HARDWARE_INTERFACE_H
+#define HARDWARE_INTERFACE_H
+
 #include <hardware_interface/joint_command_interface.h>
 #include <hardware_interface/joint_state_interface.h>
 #include <hardware_interface/robot_hw.h>
 
-class MyRobot : public hardware_interface::RobotHW
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Quaternion.h>
+#include <std_msgs/Float64.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
+#include <ros/ros.h>
+
+namespace robot_hardware_interface
 {
-public:
-  MyRobot() 
- { 
-   // connect and register the joint state interface
-   hardware_interface::JointStateHandle state_handle_a("jointA", &pos[0], &vel[0], &eff[0]);
-   jnt_state_interface.registerHandle(state_handle_a);
+  double data_[2];
 
-   hardware_interface::JointStateHandle state_handle_b("jointB", &pos[1], &vel[1], &eff[1]);
-   jnt_state_interface.registerHandle(state_handle_b);
+  class MyRobot : public hardware_interface::RobotHW
+  {
+  public:
+    MyRobot(ros::NodeHandle& nh);
+    ~MyRobot();
+    ros::Time getTime() const {return ros::Time::now();}
+    ros::Duration getPeriod() const {return ros::Duration(0.01);}
+    void read(){
+      //ROS_INFO_STREAM("Commands for joints: " << cmd_[0] << ", " << cmd_[1]);
+    }
 
-   registerInterface(&jnt_state_interface);
-
-   // connect and register the joint position interface
-   hardware_interface::JointHandle pos_handle_a(jnt_state_interface.getHandle("jointA"), &cmd[0]);
-   jnt_pos_interface.registerHandle(pos_handle_a);
-
-   hardware_interface::JointHandle pos_handle_b(jnt_state_interface.getHandle("jointB"), &cmd[1]);
-   jnt_pos_interface.registerHandle(pos_handle_b);
-
-   registerInterface(&jnt_pos_interface);
-  }
+    void write(ros::Time t);
+    void getOdometryFromArduino(const geometry_msgs::Twist::ConstPtr& twist_msg);
 
 
-ros::Time getTime() const {return ros::Time::now();}
-ros::Duration getPeriod() const {return ros::Duration(0.01);}
+  private:
+    const double WHEEL_SEPARATION = 0.6;
 
-private:
-  hardware_interface::JointStateInterface jnt_state_interface;
-  hardware_interface::PositionJointInterface jnt_pos_interface;
-  double cmd[2];
-  double pos[2];
-  double vel[2];
-  double eff[2];
-};
+    ros::NodeHandle nh_;
+    
+
+    ros::Publisher right_vel_cmd_pub_;
+    ros::Publisher left_vel_cmd_pub_;
+    ros::Subscriber sub_;
+
+    
+
+    std_msgs::Float64 rightWheelVelocity_cmd;
+    std_msgs::Float64 leftWheelVelocity_cmd;
+
+    hardware_interface::JointStateInterface jnt_state_interface;
+    hardware_interface::PositionJointInterface jnt_pos_interface;
+    hardware_interface::VelocityJointInterface jnt_vel_interface;
+
+    double lastTime;
+    double odometryLastTime;
+    //ros::Time current_time;
+
+    int publishData;
+
+    double cmd_[2];
+    double pos_[2];
+    double vel_[2];
+    double eff_[2];
+
+    double x_;
+    double y_;
+    double th_;
+
+    ros::Publisher odom_pub;
+    tf::TransformBroadcaster odom_broadcaster;
+    geometry_msgs::Quaternion odom_quat;
+    geometry_msgs::TransformStamped odom_trans;
+    nav_msgs::Odometry odom;
+  };
+}
+#endif
--- src/controller.cpp
+++ src/controller.cpp
@@ -1,14 +1,14 @@
 #include <ros/ros.h>
 #include <controller_manager/controller_manager.h>
 
-#include "hardware_interface.hpp"
+#include <dif_drv_robot/hardware_interface.hpp>
 
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "controller");
 	ros::NodeHandle nh;
 
-  	MyRobot robot;
+  	robot_hardware_interface::MyRobot robot(nh);
   	controller_manager::ControllerManager cm(&robot);
 
   	ros::Rate rate(1.0 / robot.getPeriod().toSec());
@@ -19,7 +19,7 @@ int main(int argc, char** argv)
   {
      robot.read();
      cm.update(robot.getTime(), robot.getPeriod());
-     robot.write();
+     robot.write(robot.getTime());
      rate.sleep();
   }
 
